12b4869c30b105f0e00fdd55a57bf78f588753e1
Marcus Hert da Corégio <marcusdacoregio@gmail.com>
2019-05-17 18:38:34 -0300

    Creates the project

---
+++ src/main/java/coregio/marcus/numberconverterapi/NumberConverterApiApplication.java
@@ -0,0 +1,13 @@
+package coregio.marcus.numberconverterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberConverterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberConverterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coregio/marcus/numberconverterapi/NumberConverterApiApplicationTests.java
@@ -0,0 +1,16 @@
+package coregio.marcus.numberconverterapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NumberConverterApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
