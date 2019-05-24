2106f98cf4fc57d53bd919b86d60dcad160afae9
Marcus Hert da Corégio <marcusdacoregio@gmail.com>
2019-05-24 11:27:09 -0300

    Add some whitespaces for identation

--- src/main/java/coregio/marcus/numberconverterapi/exception/InvalidDecimalForRomanConversionException.java
+++ src/main/java/coregio/marcus/numberconverterapi/exception/InvalidDecimalForRomanConversionException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class InvalidDecimalForRomanConversionException extends RuntimeException{
+public class InvalidDecimalForRomanConversionException extends RuntimeException {
 
     public InvalidDecimalForRomanConversionException(String message) {
         super(message);
